2457bba8c3abcaea66816e56c34571f4622f6234
Roy Inganta Ginting <if09062@gmail.com>
2012-12-11 21:40:53 +0700

    separate random function into new file utils.js

--- js/SudokuModel.js
+++ js/SudokuModel.js
@@ -8,8 +8,8 @@ function SudokuModel(sudokuValues){
 		var areaNumber, typeNumber, i;
 		
 		for (i=0; i<limit; i++){
-			areaNumber = this.generateArea();
-			typeNumber = this.generateType();
+			areaNumber = generateArea();
+			typeNumber = generateType();
 			this.swapLineInArea(areaNumber, typeNumber);
 		}
 	}
@@ -20,13 +20,13 @@ function SudokuModel(sudokuValues){
 		this.maskedSpec = new SudokuSpec(spec);
 		// random position in a row that will be masked
 		for (i=0; i<9; i++){
-			selectedRows = this.generateTwoNumbers();
+			selectedRows = generateTwoNumbers();
 			this.maskedSpec.setMaskAt(i, selectedRows[0]);
 			this.maskedSpec.setMaskAt(i, selectedRows[1]);
 		}
 		// random position in a column that will be masked
 		for (i=0; i<9; i++){
-			temp = this.generateUniqueNumber(this.maskedSpec.getMaskedColumn(i));
+			temp = generateUniqueNumber(this.maskedSpec.getMaskedColumn(i));
 			this.maskedSpec.setMaskAt(temp, i);
 		}
 	}
@@ -43,30 +43,6 @@ function SudokuModel(sudokuValues){
 			this.spec.swapRow(pos[0], pos[1]);
 		}
 	}
-	// generate random number between 0 (inclusive) and limit (exclusive)
-	this.rand = function(limit){
-		return Math.floor(Math.random() * limit);
-	}
-	// Generate two different number between 0 (inclusive) and 9 (exclusive)
-	this.generateTwoNumbers = function(){
-		var temp, numbers = new Array();
-
-		numbers.push(this.rand(9));
-		do{
-			temp = this.rand(9);
-		} while (temp == numbers[0]);
-		numbers.push(temp);
-		return numbers;
-	}
-	// Generate number which is not equal with given values
-	this.generateUniqueNumber = function(comparator){
-		var temp;
-		
-		do{
-			temp = this.rand(9);
-		} while (comparator.indexOf(temp) != -1);
-		return temp;
-	}
 	// Calculate index from areaNumber and typeNumber
 	this.getIndex = function(areaNumber, typeNumber){
 		var result = new Array();
@@ -78,14 +54,6 @@ function SudokuModel(sudokuValues){
 	// Check whether given area number is located on column or not
 	this.isColumn = function(areaNumber){
 		return areaNumber / this.spec.interval < 1;
-	}
-	// Generate valid random type
-	this.generateType = function(){
-		return this.rand(this.spec.interval);
-	}
-	// Generate valid random area number
-	this.generateArea = function(){
-		return this.rand(2*this.spec.interval);
 	}
 	// output to console
 	this.toConsole = function(){
